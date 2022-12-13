e4a3d2b24f0bb1af5d007592ca86c435b0159a66
sakhawat9 <sakhawathossain7969@gmail.com>
2022-12-13 13:21:36 +0600

    add jsconfig.json

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import { FaSignInAlt, FaSignOutAlt } from "react-icons/fa";
-import styles from "../styles/Header.module.css";
+import styles from "@/styles/Header.module.css";
 
 export default function Header() {
   return (
