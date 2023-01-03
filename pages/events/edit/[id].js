da2c455bafe1fac03c2b3edf928e2ac1683fe9b4
sakhawat9 <sakhawathossain7969@gmail.com>
2023-01-03 21:48:21 +0600

    update

--- pages/events/edit/[id].js
+++ pages/events/edit/[id].js
@@ -1,88 +1,157 @@
-import moment from "moment";
-import { parseCookies } from "@/helpers/index";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { useState } from "react";
 import { useRouter } from "next/router";
 import Link from "next/link";
 import Layout from "@/components/Layout";
 import { API_URL } from "@/config/index";
 import styles from "@/styles/Form.module.css";
 import Image from "next/image";
-import { FaImage } from "react-icons/fa";
-import Modal from "@/components/Modal";
-import ImageUpload from "@/components/ImageUpload";
+import { useFormik } from "formik";
 
 export default function EditEventPage({ evt }) {
-  const { name, performers, venue, address, data, time, description, image } =
+  const { name, performers, venue, address, date, time, description, image } =
     evt.data.attributes;
+  let router = useRouter();
+  const formik = useFormik({
+    initialValues: {
+      name: name,
+      performers: performers,
+      venue: venue,
+      address: address,
+      date: date,
+      time: time,
+      description: description,
+    },
 
-  const [values, setValues] = useState({
-    name: name,
-    performers: performers,
-    venue: venue,
-    address: address,
-    date: data,
-    time: time,
-    description: description,
+    onSubmit: async (values) => {
+      const formData = new FormData();
+      const data = {
+        name: values.name,
+        performers: values.performers,
+        venue: values.venue,
+        address: values.address,
+        date: values.date,
+        time: values.time,
+        description: values.description,
+      };
+
+      // Validation
+      const hasEmptyFields = Object.values(values).some(
+        (element) => element === ""
+      );
+
+      if (hasEmptyFields) {
+        toast.error("Please fill in all fields");
+      }
+      formData.append("data", JSON.stringify(data));
+
+      //image-file
+      formData.append("files.image", values.imageFile);
+
+      //for upload page
+      const uploadData = new FormData();
+      uploadData.append("files", values.imageFile);
+
+      //update collection
+      const res = await fetch(
+        `http://localhost:1337/api/events/${evt.data.id}`,
+        {
+          method: "PUT",
+          body: formData,
+          headers: {},
+        }
+      );
+
+      if (!res.ok) {
+        if (res.status === 403 || res.status === 401) {
+          toast.error("No token included");
+          return;
+        }
+        toast.error("Something Went Wrong");
+      } else {
+        const evt = await res.json();
+        router.push(`/events/${evt.data.attributes.slug}`);
+      }
+      //upload file to uploads
+      const uploadFile = await fetch("http://localhost:1337/api/upload", {
+        method: "POST",
+        body: uploadData,
+        headers: {},
+      });
+    },
   });
 
-  const [imagePreview, setImagePreview] = useState(
-    image.data !== null ? image.data?.attributes.formats.thumbnail.url : null
-  );
-
-  const [showModal, setShowModal] = useState(false);
-
-  const router = useRouter();
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    // Validation
-    const hasEmptyFields = Object.values(values).some(
-      (element) => element === ""
-    );
-
-    if (hasEmptyFields) {
-      toast.error("Please fill in all fields");
-    }
-
-    const res = await fetch(`${API_URL}/events/${evt.data.id}`, {
-      method: "PUT",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ data: values }),
-    });
-
-    if (!res.ok) {
-      // if (res.status === 403 || res.status === 401) {
-      //   toast.error("No token included");
-      //   return;
-      // }
-      toast.error("Something Went Wrong");
-    } else {
-      const evt = await res.json();
-      router.push(`/events/${evt.data.attributes.slug}`);
-    }
+  const imageUpload = (e) => {
+    const file = e.target.files;
+    formik.setFieldValue("imageFile", file[0]);
   };
 
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setValues({ ...values, [name]: value });
-  };
+  // const [values, setValues] = useState({
+  //   name: name,
+  //   performers: performers,
+  //   venue: venue,
+  //   address: address,
+  //   date: data,
+  //   time: time,
+  //   description: description,
+  // });
 
-  const imageUploaded = async (e) => {
-    const res = await fetch(`${API_URL}/events/${evt.data.id}`);
-    const data = await res.json();
-    setImagePreview(image.data.attributes.formats.thumbnail.url);
-    setShowModal(false);
-  };
+  // const [imagePreview, setImagePreview] = useState(
+  //   image.data !== null ? image.data?.attributes.formats.thumbnail.url : null
+  // );
+
+  // const [showModal, setShowModal] = useState(false);
+
+  // const router = useRouter();
+
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
+
+  //   // Validation
+  //   const hasEmptyFields = Object.values(values).some(
+  //     (element) => element === ""
+  //   );
+
+  //   if (hasEmptyFields) {
+  //     toast.error("Please fill in all fields");
+  //   }
+
+  //   const res = await fetch(`${API_URL}/events/${evt.data.id}`, {
+  //     method: "PUT",
+  //     headers: { "Content-Type": "application/json" },
+  //     body: JSON.stringify({ data: values }),
+  //   });
+
+  //   if (!res.ok) {
+  //     // if (res.status === 403 || res.status === 401) {
+  //     //   toast.error("No token included");
+  //     //   return;
+  //     // }
+  //     toast.error("Something Went Wrong");
+  //   } else {
+  //     const evt = await res.json();
+  //     router.push(`/events/${evt.data.attributes.slug}`);
+  //   }
+  // };
+
+  // const handleInputChange = (e) => {
+  //   const { name, value } = e.target;
+  //   setValues({ ...values, [name]: value });
+  // };
+
+  // const imageUploaded = async (e) => {
+  //   const res = await fetch(`${API_URL}/events/${evt.data.id}`);
+  //   const data = await res.json();
+  //   setImagePreview(image.data.attributes.formats.thumbnail.url);
+  //   setShowModal(false);
+  // };
 
   return (
     <Layout title="Add New Event">
       <Link href="/events">Go Back</Link>
       <h1>Edit Event</h1>
       <ToastContainer />
-      <form onSubmit={handleSubmit} className={styles.form}>
+      <form onSubmit={formik.handleSubmit} className={styles.form}>
         <div className={styles.grid}>
           <div>
             <label htmlFor="name">Event Name</label>
@@ -90,8 +159,8 @@ export default function EditEventPage({ evt }) {
               type="text"
               id="name"
               name="name"
-              value={values.name}
-              onChange={handleInputChange}
+              onChange={formik.handleChange}
+              value={formik.values.name}
             />
           </div>
           <div>
@@ -100,8 +169,8 @@ export default function EditEventPage({ evt }) {
               type="text"
               name="performers"
               id="performers"
-              value={values.performers}
-              onChange={handleInputChange}
+              onChange={formik.handleChange}
+              value={formik.values.performers}
             />
           </div>
           <div>
@@ -110,8 +179,8 @@ export default function EditEventPage({ evt }) {
               type="text"
               name="venue"
               id="venue"
-              value={values.venue}
-              onChange={handleInputChange}
+              onChange={formik.handleChange}
+              value={formik.values.venue}
             />
           </div>
           <div>
@@ -120,8 +189,8 @@ export default function EditEventPage({ evt }) {
               type="text"
               name="address"
               id="address"
-              value={values.address}
-              onChange={handleInputChange}
+              onChange={formik.handleChange}
+              value={formik.values.address}
             />
           </div>
           <div>
@@ -130,8 +199,8 @@ export default function EditEventPage({ evt }) {
               type="date"
               name="date"
               id="date"
-              value={moment(values.date).format("yyyy-MM-DD")}
-              onChange={handleInputChange}
+              onChange={formik.handleChange}
+              value={formik.values.date}
             />
           </div>
           <div>
@@ -140,8 +209,8 @@ export default function EditEventPage({ evt }) {
               type="text"
               name="time"
               id="time"
-              value={values.time}
-              onChange={handleInputChange}
+              onChange={formik.handleChange}
+              value={formik.values.time}
             />
           </div>
         </div>
@@ -152,46 +221,43 @@ export default function EditEventPage({ evt }) {
             type="text"
             name="description"
             id="description"
-            value={values.description}
-            onChange={handleInputChange}
+            onChange={formik.handleChange}
+            value={formik.values.description}
           ></textarea>
         </div>
-
-        <input type="submit" value="Update Event" className="btn" />
+        <div>
+          <label htmlFor="image">image-file-upload</label>
+          <input
+            id="file"
+            name="file"
+            type="file"
+            onChange={imageUpload}
+            value={formik.values.image}
+          />
+        </div>
+        <button className="btn" type="submit">
+          Add Event
+        </button>
       </form>
 
       <h2>Event Image</h2>
-      {imagePreview ? (
-        <Image src={imagePreview} height={100} width={170} alt={name} />
+      {image.data !== null ? (
+        <Image
+          src={image.data.attributes.formats.thumbnail.url}
+          height={100}
+          width={170}
+          alt={formik.values.name}
+        />
       ) : (
         <div>
           <p>No image uploaded</p>
         </div>
       )}
-
-      <div>
-        <button
-          onClick={() => setShowModal(true)}
-          className="btn-secondary btn-icon"
-        >
-          <FaImage /> Set Image
-        </button>
-      </div>
-
-      <Modal show={showModal} onClose={() => setShowModal(false)}>
-        <ImageUpload
-          evtId={evt.data.id}
-          imageUploaded={imageUploaded}
-          // token={token}
-        />
-      </Modal>
     </Layout>
   );
 }
 
 export async function getServerSideProps({ params: { id }, req }) {
-  console.log("id", id);
-
   const res = await fetch(`${API_URL}/events/${id}?populate=*`);
   const evt = await res.json();
 
